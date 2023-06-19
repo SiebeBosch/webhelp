3decaabb088513b8e25bf39983972819d07fa9e9
Siebe Bosch <siebe@hydroconsult.nl>
2023-06-19 22:03:58 +0200

    styling

--- js/script.js
+++ js/script.js
@@ -1,16 +1,43 @@
 let currentLanguage = 'EN'; // This can be 'EN' or 'NL'
 
-// Function to handle language toggle
-function toggleLanguage(lang) {
-  language = lang;
-  renderContent();
-}
-
 document.getElementById("flagEN").addEventListener("click", () => toggleLanguage("EN"));
 document.getElementById("flagNL").addEventListener("click", () => toggleLanguage("NL"));
 
+function toggleLanguage(lang) {
+  currentLanguage = lang;
+  renderContent();
+}
+
+function createChapter(chapter) {
+  let chapterDiv = document.createElement('div');
+  let title = document.createElement('h2');
+  title.textContent = chapter.title[currentLanguage];
+  chapterDiv.appendChild(title);
+
+  chapter.paragraphs.forEach(paragraph => {
+    let paragraphDiv = document.createElement('div');
+
+    if (paragraph.title) { // Check if title exists
+      let paragraphTitle = document.createElement('h3');
+      paragraphTitle.textContent = paragraph.title[currentLanguage];
+      paragraphDiv.appendChild(paragraphTitle);
+    }
+
+    let text = document.createElement('p');
+    if (paragraph.text) { // If "text" exists
+      text.textContent = paragraph.text[currentLanguage];
+    } else if (paragraph[currentLanguage]) { // If it's a paragraph directly
+      text.textContent = paragraph[currentLanguage];
+    }
+
+    paragraphDiv.appendChild(text);
+    chapterDiv.appendChild(paragraphDiv);
+  });
+
+  return chapterDiv;
+}
+
 function renderContent() {
-  // Handle rendering content based on the current language
   let contentsDiv = document.getElementById('tableOfContents');
   let mainPanelDiv = document.getElementById('mainPanel');
 
@@ -22,46 +49,13 @@ function renderContent() {
     // Add title to contentsDiv
     let title = document.createElement('h2');
     title.innerHTML = content.chapters[i].title[currentLanguage];
-    contentsDiv.appendChild(title);
+    title.addEventListener('click', function() {
+      mainPanelDiv.innerHTML = '';
+      mainPanelDiv.appendChild(createChapter(content.chapters[i]));
+    });
 
-    // Add paragraphs to mainPanelDiv
-    for (let j = 0; j < content.chapters[i].paragraphs.length; j++) {
-      let para = document.createElement('p');
-      para.innerHTML = content.chapters[i].paragraphs[j][currentLanguage];
-      mainPanelDiv.appendChild(para);
-    }
+    contentsDiv.appendChild(title);
   }
 }
 
-function createChapter(chapter) {
-    let chapterDiv = document.createElement('div');
-    let title = document.createElement('h2');
-    title.textContent = chapter.title[currentLanguage];
-    chapterDiv.appendChild(title);
-    
-    chapter.paragraphs.forEach(paragraph => {
-      let paragraphDiv = document.createElement('div');
-  
-      if(paragraph.title) { // Check if title exists
-        let paragraphTitle = document.createElement('h3');
-        paragraphTitle.textContent = paragraph.title[currentLanguage];
-        paragraphDiv.appendChild(paragraphTitle);
-      }
-  
-      let text = document.createElement('p');
-      if(paragraph.text) { // If "text" exists
-        text.textContent = paragraph.text[currentLanguage];
-      } else if(paragraph.EN) { // If it's a paragraph directly
-        text.textContent = paragraph[currentLanguage];
-      }
-  
-      paragraphDiv.appendChild(text);
-      chapterDiv.appendChild(paragraphDiv);
-    });
-    
-    return chapterDiv;
-  }
-
 renderContent(); // Call the function to render the content initially
-
-//document.getElementById('mainPanel').appendChild(createChapter(content.chapters[0])); // For testing
